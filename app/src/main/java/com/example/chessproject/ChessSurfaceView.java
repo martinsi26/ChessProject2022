4cc4815a587e6bd6110f2ae650c8519ef025552f
basestations <santosme23@up.edu>
2022-09-25 16:16:20 -0700

    added a singular square

--- app/src/main/java/com/example/chessproject/ChessSurfaceView.java
+++ app/src/main/java/com/example/chessproject/ChessSurfaceView.java
@@ -5,29 +5,27 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.AttributeSet;
 import android.view.SurfaceView;
 
 public class ChessSurfaceView extends SurfaceView {
 
-    //private Paint black;
-    //private Rect newRect;
+    private Paint black;
+    private Rect newRect;
 
-    public ChessSurfaceView(Context context) {
-        super(context);
+    public ChessSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
         setWillNotDraw(false);
 
-        //newRect = new Rect(10,10,50,50);
+        newRect = new Rect(100,100,200,200);
 
-        //black = new Paint();
-        //black.setColor(Color.BLACK);
+        black = new Paint();
+        black.setColor(Color.WHITE);
 
     }
 
-    @Override
     protected void onDraw(Canvas canvas) {
-        //super.onDraw(canvas);
-        //canvas.drawRect(newRect, black);
-
-
+        super.onDraw(canvas);
+        canvas.drawRect(newRect, black);
     }
 }
--- app/src/main/java/com/example/chessproject/MainActivity.java
+++ app/src/main/java/com/example/chessproject/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //ChessSurfaceView chessSurfaceView = findViewById(R.id.chessBoard);
+        ChessSurfaceView chessSurfaceView = findViewById(R.id.chessBoard);
 
 
     }
