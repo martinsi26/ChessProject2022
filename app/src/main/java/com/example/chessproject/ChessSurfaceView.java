6a6d9ca1886bac92d8cd21560d70ea40e8523adc
basestations <santosme23@up.edu>
2022-09-25 17:10:12 -0700

    added chess board

--- app/src/main/java/com/example/chessproject/ChessSurfaceView.java
+++ app/src/main/java/com/example/chessproject/ChessSurfaceView.java
@@ -10,23 +10,55 @@ import android.view.SurfaceView;
 
 public class ChessSurfaceView extends SurfaceView {
 
-    private Paint black;
+    private Paint colorSquare;
     private Rect newRect;
+
+    private float top;
+    private float left;
+
+    private float bottom;
+    private float right;
+
+    private float size;
+
+
 
     public ChessSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
 
-        newRect = new Rect(100,100,200,200);
+        size = 75;
 
+        left = 100;
+        top = 30;
 
-        black = new Paint();
-        black.setColor(Color.WHITE);
+        right = left + size;
+        bottom = top + size;
+
+        //newRect = new Rect(100,100,200,200);
+
+        colorSquare = new Paint();
+        colorSquare.setColor(Color.WHITE);
 
     }
 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawRect(newRect, black);
+
+        //board initialization
+        for(int j = 1; j <= 8; j++) {
+            for(int i = 1; i <= 8; i++) {
+                //alternate colors
+                if((i%2 == 0 && j%2 != 0) || (j%2 == 0 && i%2 != 0)){
+                    colorSquare.setColor(Color.BLACK);
+                } else {
+                    colorSquare.setColor(Color.WHITE);
+                }
+
+                //draw rectangle
+                canvas.drawRect(left + (right - left) * i, top + (bottom - top) * j,
+                        right + (right - left) * i, bottom + (bottom - top) * j, colorSquare);
+            }
+        }
     }
 }
