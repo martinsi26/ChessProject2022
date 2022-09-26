6a8b79e5d5448b9bd054a618d5c87d28e1692b5b
cachola25 <cachola25@up.edu>
2022-09-25 19:29:11 -0700

    Added captured black pieces and changed captured piece background to grey

--- app/src/main/java/com/example/chessproject/BlackCapturesSurfaceView.java
+++ app/src/main/java/com/example/chessproject/BlackCapturesSurfaceView.java
@@ -11,18 +11,31 @@ import android.view.SurfaceView;
 
 public class BlackCapturesSurfaceView extends SurfaceView {
 
-    private Paint imagePaint;
-
+    private Bitmap pawnImage;
+    private Bitmap knightImage;
+    private Bitmap bishopImage;
 
     public BlackCapturesSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
 
-        imagePaint = new Paint();
-        imagePaint.setColor(Color.BLACK);
+        pawnImage = BitmapFactory.decodeResource(getResources(),R.drawable.bp);
+        knightImage = BitmapFactory.decodeResource(getResources(),R.drawable.bn);
+        bishopImage = BitmapFactory.decodeResource(getResources(),R.drawable.bb);
+        pawnImage = Bitmap.createScaledBitmap(pawnImage,120,100,false);
+        knightImage = Bitmap.createScaledBitmap(knightImage,120,100,false);
+        bishopImage = Bitmap.createScaledBitmap(bishopImage,120,100,false);
     }
 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        Paint imagePaint = new Paint();
+        imagePaint.setColor(Color.WHITE);
+        canvas.drawBitmap(pawnImage,-10, 51, imagePaint);
+        canvas.drawBitmap(pawnImage,30,51,imagePaint);
+        canvas.drawBitmap(knightImage,100,51,imagePaint);
+        canvas.drawBitmap(bishopImage,195,51,imagePaint);
+
     }
 }
